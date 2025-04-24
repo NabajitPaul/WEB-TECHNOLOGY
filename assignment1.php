dee75f51280e82027120a58a769d577c6689ea9c
Nabajit Paul <149513573+NabajitPaul@users.noreply.github.com>
2025-04-24 10:44:28 +0530

    Add files via upload

---
+++ assignment1.php
@@ -0,0 +1,35 @@
+<?php
+
+$name = "Bhargab"; //string
+$age = 20; //integer
+$height = 6.2; //float
+$boolean = true; //boolean
+$array = ["apple" , "banana" , "mango"] ; //array
+$details = ["Name"=>"Bhargab","Roll No"=>"CSB22044","Age"=>"20"];
+
+
+
+echo "My name is $name and <br>";
+echo "My age is $age <br>";
+echo "My height is $height <br>";
+echo "I am a student of cse is $boolean ; (i.e) true <br>";
+echo "An sequence of numbers is called as: ";
+print_r($array);
+
+echo "$array[0] <br>";
+echo $details["Name"];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
